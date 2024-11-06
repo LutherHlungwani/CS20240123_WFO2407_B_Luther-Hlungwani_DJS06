65473fddbc896d620a2ba647cddd56f49388aa70
Luther Hlungwani <lhhlungwani3@gmail.com>
2024-11-06 16:48:37 +0200

    higher order functions coded

--- scripts.js
+++ scripts.js
@@ -13,3 +13,36 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
+console.log(products.forEach(product => console.log(product.product)));
+
+console.log(products.filter(product => product.product.length <= 5));
+
+console.log(
+  products
+  .filter(product => product.price && !isNaN(product.price))
+  .map(product => Number(product.price))
+  .reduce((total, price) => toal + price, 0)
+);
+
+console.log(product.reduce((acc, product) => acc + product.product, ''));
+
+console.log(
+  products
+  .filter(product => product.price && !isNaN(product.price))
+  .map(product => ({ ...product, price: Number(product.price) }))
+  .reduce(
+    (acc, product) => ({
+      highest: Math.max(acc.highest, product.price),
+      lowest: Math.min(acc.lowest, product.price)
+    }),
+    { highest: -Infinity, lowest: Infinity}
+  )
+)
+
+console.log(
+  Object.entries(products).reduce((acc, [_, {product, price}]) =>{
+    acc.push({name: product, cost: price});
+    return acc;
+  }, [])
+);
